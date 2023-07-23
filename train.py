ee3ebe16d0b2e03747aeb89b11530f48257fc0d8
Alexei Chmelev <alexei_chmelev@gmx.de>
2023-07-23 08:20:45 +0200

    added a config file

--- train.py
+++ train.py
@@ -9,29 +9,38 @@ from numbercode import NumberCode
 from dataset import NumberCodeDataSet
 from model import NumberCodeModel 
 from torch.utils.data import DataLoader
+from configparser import ConfigParser
 
 if torch.cuda.is_available():
-  print("CUDA is available, running on it")
-  device = torch.device("cuda")
+  print("CUDA is available")
+  enviroment = 'cuda'
 else:
-  print("CUDA isn't available, running on CPU")
-  device = torch.device("cpu")
+  enviroment = 'nocuda'
+  print("CUDA isn't available")
 
-batch_size = 16
-trainset = NumberCodeDataSet(100000)
+config = ConfigParser()
+config.read('train.conf')
+
+deviceName = config.get(enviroment,'device')   
+device = torch.device(deviceName)
+
+batch_size = config.getint(enviroment, 'batch_size')
+trainset = NumberCodeDataSet(config.getint(enviroment, 'dataset_size'))
 loader = DataLoader(trainset, batch_size = batch_size, shuffle = True)
-model = NumberCodeModel(hiddenLayerOrder=4)
+model = NumberCodeModel(hiddenLayerOrder=config.getint(enviroment, 'hidden_layer_size'))
 model.to(device)
 model.init_origin()
 
 criterion = nn.CrossEntropyLoss()
 ##optimizer = optim.SGD(model.parameters(), lr=10.0)
-optimizer = optim.Adam(model.parameters(), 0.001)
+lr = config.getfloat(enviroment, 'learning_rate')
+optimizer = optim.Adam(model.parameters(), lr)
 
-numberOfEpochs = 1000
+numberOfEpochs = config.getint(enviroment, 'epochs')
 min_validation_loss = 10.0
 epochCounter = 0
 trainingTime = 0.0
+print('Training '+str(numberOfEpochs)+" epochs on "+deviceName+" with learning rate "+str(lr))
 for epoch in range(numberOfEpochs):  # loop over the dataset multiple times
 
     ##Training loop 
@@ -53,7 +62,7 @@ for epoch in range(numberOfEpochs):  # loop over the dataset multiple times
         optimizer.step()
 
         running_loss += loss.item()
-        batchGroupSize = 2000
+        batchGroupSize = config.getint(enviroment, 'batch_group_size')
         if i % batchGroupSize == (batchGroupSize -1):    # print every batchGroupSize mini-batches
             print("#############################################################################")
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / batchGroupSize:.3f}')
@@ -71,7 +80,7 @@ for epoch in range(numberOfEpochs):  # loop over the dataset multiple times
     ##Validation loop
     validation_loss = 0.0
     counter = 0
-    validation_set = NumberCodeDataSet(10000)
+    validation_set = NumberCodeDataSet(config.getint(enviroment, 'dataset_size'))
     validation_loader = DataLoader(validation_set, batch_size = batch_size, shuffle = True)
     for data in validation_loader:
         input = data['input']
